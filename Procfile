fdfc34af404b3ca5cd67a79cf0a067719fa1eff5
kartik-soni1707 <its.kartiksoni@gmail.com>
2020-07-15 20:09:44 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn WebDev.wsgi --log-file -
--- WebDev/settings.py
+++ WebDev/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'imjse*tz+x+vw(ol&i3uh4nfhrs*)+-b&vvl0c6bl)x3&29vzy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['basicaaaaaaa.herokuapp.com','127.0.0.1']
 
 
 # Application definition
